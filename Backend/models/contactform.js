2b70cbb5fad621a254ffaeaa5b0301e07af6e91c
Ben <bennywilson05@gmail.com>
2026-09-17 19:41:14 -0400

    Created Schema for Contact and Request forms.

---
+++ Backend/models/contactform.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const contactFormSchema = new Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    
+    phone: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const ContactForm = mongoose.model("ContactForm", contactFormSchema);
+
+module.exports = ContactForm;
+
+
---
+++ Backend/models/requestforms.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const requestFormSchema = new Schema({
+    fname: {
+        type: String,
+        required: true 
+    },
+    lname: {
+        type: String,
+        required: true
+
+    },
+    phone: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: false
+    },
+    zipcode: {
+        type: String,
+        required: true
+    },
+    project: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const RequestForm = mongoose.model("RequestForm", requestFormSchema);
+
+module.exports = RequestForm;
